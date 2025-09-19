f7ce5a6004e256fccb8cb952908038d128105108
Andrew Yan <hello.andrew.yan@outlook.com>
2025-09-19 23:14:46 +1000

    feat: Implementing constants for encode.py

---
+++ utils/encode.py
@@ -0,0 +1,9 @@
+KEYS = ['W', 'A', 'S', 'D', 'Space', 'Shift', 'Q', 'E', 'R', 'F', 'C', 'V']
+
+MOUSE_BUTTONS = ['left', 'right']
+
+TYPE_INDEX = {
+    'KeyPress': 0,
+    'MousePosition': 1,
+    'MouseClick': 2
+}
